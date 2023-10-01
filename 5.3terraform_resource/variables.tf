19d8d5238e40e96428b12fbd8a26d544fd9099b5
yihao.sun <matrix.abelrose@gmail.com>
2023-10-01 17:15:54 +0800

    [feat]: variable

---
+++ 5.3terraform_resource/variables.tf
@@ -0,0 +1,17 @@
+variable REGION {
+  type = string
+  default = ""
+  default = "REGION"
+}
+
+variable ALICLOUD_ACCESS_KEY {
+  type        = string
+  default     = ""
+  description = "ALICLOUD_ACCESS_KEY"
+}
+
+variable ALICLOUD_SECRET_KEY {
+  type        = string
+  default     = ""
+  description = "ALICLOUD_SECRET_KEY"
+}
