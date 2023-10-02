9d7746212c8cdc142d934b802707556943101e54
yihao.sun <matrix.abelrose@gmail.com>
2023-10-02 21:12:11 +0800

    [feat]: Terraform资源依赖

--- 9.1terraform_depend_on/main.tf
+++ 9.1terraform_depend_on/main.tf
@@ -5,7 +5,6 @@ provider "alicloud" {
   secret_key = var.ALICLOUD_SECRET_KEY
 }
 
-
 #创建vpc
 resource "alicloud_vpc" "vpc" {
   vpc_name   = "vpc_1"
@@ -13,7 +12,6 @@ resource "alicloud_vpc" "vpc" {
 }
 
 # 创建vswitch
-# alicloud_vswitch是阿里云的资源字段，vsw_1字段是tf文件中的自定义唯一资源名称,vswitch_name字段是在阿里云上的自定义备注名
 resource "alicloud_vswitch" "vsw_1" {
   vswitch_name = "vsw_aliyun1"
   vpc_id       = alicloud_vpc.vpc.id
@@ -39,7 +37,6 @@ resource "alicloud_security_group_rule" "nsg_rule1" {
   cidr_ip           = "0.0.0.0/0"
 }
 
-
 #创建ECS实例
 resource "alicloud_instance" "instance" {
   # cn-shanghai
@@ -52,4 +49,29 @@ resource "alicloud_instance" "instance" {
   vswitch_id                 = alicloud_vswitch.vsw_1.id
   internet_max_bandwidth_out = 5
   password                   = "5jejYWzSjZhWQc7G22"
+}
+
+resource "alicloud_oss_bucket" "ossbucket1" {
+  bucket = "ossbucket1-kevin-aptx4869-ikun1"
+  acl    = "private"
+  # depends_on 表示必须创建完 instance 之后才能创建 ossbucket1
+  depends_on = [
+    alicloud_instance.instance
+  ]
+}
+
+resource "alicloud_oss_bucket" "ossbucket2" {
+  bucket = "ossbucket1-kevin-aptx4869-niganma2"
+  acl    = "private"
+  depends_on = [
+    alicloud_oss_bucket.ossbucket1
+  ]
+}
+
+resource "alicloud_oss_bucket" "ossbucket3" {
+  bucket = "ossbucket1-kevin-aptx4869-zhiyinnitaimei3"
+  acl    = "private"
+  depends_on = [
+    alicloud_oss_bucket.ossbucket2
+  ]
 }
